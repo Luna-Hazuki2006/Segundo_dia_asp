b9da25238b348ee8f66bff9e44899c43450a35ad
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-26 20:00:25 -0400

    con la base

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424040bb3d95926a9b6f5028b7f979beaa0255f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063f7d1df3d12a441755932f78e5f5b0e8f9f566")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
