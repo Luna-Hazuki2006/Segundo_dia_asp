a607ac59fd53ed5d326ab1a5f94d9336f9b04d59
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-27 13:17:37 -0400

    if hell is forever

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7f3777c1d8d48bfca54a562035622c0adeab3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37323dc6d6fd313f15e50336f4404acdb67bdc04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
