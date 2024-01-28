b35a0dfe0ad7aa5ebdb443b9547ace6cb3703ea3
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-28 15:34:51 -0400

    adfawdfsd

--- Core/Repositorios/IPersonajeRepositorio.cs
+++ Core/Repositorios/IPersonajeRepositorio.cs
@@ -4,5 +4,8 @@ namespace Core.Repositorios {
     public interface IPersonajeRepositorio : IBaseRepositorio<Personaje>
     {
         Task LeveUp(Personaje personaje);
+        Task LosingLife(Personaje personaje, double vida_perdida);
+        Task GainingLife(Personaje personaje, double vida_ganada);
+        Task UsarMagia(Personaje personaje, double magia);
     }
 }
