301eedcde8e3b99bc5ba57315db15943867176d0
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-05 13:41:08 -0400

    el core está en teoría completo

--- Core/Repositorios/IBaseRepositorio.cs
+++ Core/Repositorios/IBaseRepositorio.cs
@@ -1,6 +1,12 @@
-namespace Core.Repositorios {
+namespace Core.Repositorios 
+{
     public interface IBaseRepositorio<TEntity> where TEntity : class
     {
-        
+        ValueTask<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        void Remove(TEntity entidad);
+        void RemoveRange(IEnumerable<TEntity> entidades);
+        Task Update(TEntity entidad);
+        Task AddAsync(TEntity entidad);
     }
 }
