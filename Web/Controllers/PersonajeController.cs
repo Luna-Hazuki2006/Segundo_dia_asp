47e602c4fa2ef79a05d58501864976f7678f468e
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-15 03:00:05 -0400

    algo

--- Web/Controllers/PersonajeController.cs
+++ Web/Controllers/PersonajeController.cs
@@ -27,7 +27,12 @@ namespace Web.Controllers
 
             return Ok(Personajes);
         }
-
+        
+        /// <summary>
+        /// Cosa que objetener
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns> 
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<Personaje>>> Get(int id){
 
@@ -37,6 +42,22 @@ namespace Web.Controllers
         }
 
         // POST api/<PersonajeController>
+        /// <summary>
+        /// Cosas a actualizar
+        /// </summary>
+        /// <param name="id">Prueba</param>
+        /// <returns>Algo para devolver</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item #1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
         [HttpPost]
         public async Task<ActionResult<Personaje>> Post([FromBody] Personaje personaje)
         {
