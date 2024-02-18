ab916c927729e7dcd86441cd21e888c8211be99c
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-18 10:14:12 -0400

    con recompensa

--- Web/Controllers/RecompensaController.cs
+++ Web/Controllers/RecompensaController.cs
@@ -19,9 +19,21 @@ namespace Web.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Método para devolver todos los tipos de recompensa
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Una lista de objetos de recompensa</returns>
+        /// <remarks>
+        /// Ejemplo de lista devuelta 
+        /// [
+        ///     {
+        ///         "Id": 0, 
+        ///         "Experiencia": 0.0, 
+        ///         "Objetos": [...], 
+        ///         "Enemigos": [...], 
+        ///         "Monedas": 0
+        ///     }
+        /// ]
+        /// </remarks>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Recompensa>>> Get(){
 
@@ -31,23 +43,43 @@ namespace Web.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Método para obtener una recompensa
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}")]
-        public async Task<ActionResult<IEnumerable<Recompensa>>> Get(int id){
+        /// <param name="Id">La id de la recompensa</param>
+        /// <returns>Objeto de tipo de recompensa</returns>
+        /// <remarks>
+        /// Ejemplo de objeto devuelto 
+        /// {
+        ///     "Id": 0, 
+        ///     "Experiencia": 0.0, 
+        ///     "Objetos": [...], 
+        ///     "Enemigos": [...], 
+        ///     "Monedas": 0
+        /// }
+        /// </remarks>
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<IEnumerable<Recompensa>>> Get(int Id){
 
-            var Recompensas = await _servicio.GetRecompensaById(id);
+            var Recompensas = await _servicio.GetRecompensaById(Id);
 
             return Ok(Recompensas);
         }
 
         /// <summary>
-        /// 
+        /// Método para creación de una recompensa
         /// </summary>
-        /// <param name="Recompensa"></param>
-        /// <returns></returns>
+        /// <param name="Recompensa">La instancia de la clase de recompensa</param>
+        /// <returns>Objeto de la nueva recompensa</returns>
+        /// <remarks>
+        /// Ejemplo de un Json request
+        /// {
+        ///     "Id": 0, 
+        ///     "Experiencia": 0.0, 
+        ///     "Objetos": [...], 
+        ///     "Enemigos": [...], 
+        ///     "Monedas": 0
+        /// }
+        /// </remarks>
         [HttpPost]
         public async Task<ActionResult<Recompensa>> Post([FromBody] Recompensa Recompensa)
         {
@@ -65,18 +97,28 @@ namespace Web.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Método para modificar una recompensa
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="Recompensa"></param>
-        /// <returns></returns>
-        [HttpPut("{id}")]
-        public async Task<ActionResult<Recompensa>> Put(int id, [FromBody] Recompensa Recompensa)
+        /// <param name="Id">Es la id de la recompensa a modificar</param>
+        /// <param name="Recompensa">Es el objeto de la recompensa modificada</param>
+        /// <returns>La recompensa modificada</returns>
+        /// <remarks>
+        /// Ejemplo de una recompensa devuelta 
+        /// {
+        ///     "Id": 0, 
+        ///     "Experiencia": 0.0, 
+        ///     "Objetos": [...], 
+        ///     "Enemigos": [...], 
+        ///     "Monedas": 0
+        /// }
+        /// </remarks>
+        [HttpPut("{Id}")]
+        public async Task<ActionResult<Recompensa>> Put(int Id, [FromBody] Recompensa Recompensa)
         {
             try
             {
                 var updatedRecompensa =
-                    await _servicio.UpdateRecompensa(id, Recompensa);
+                    await _servicio.UpdateRecompensa(Id, Recompensa);
 
                 return Ok(updatedRecompensa);
             }
