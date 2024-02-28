edcae39947f080f91959b6b6603261686a9dcf70
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-27 21:33:18 -0400

    the answer is no... le gasp

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92052af2e1f29694d8a2c822d18c39479a06e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d627ea46ea498c48fe5cd18227137aab6e6d13b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
