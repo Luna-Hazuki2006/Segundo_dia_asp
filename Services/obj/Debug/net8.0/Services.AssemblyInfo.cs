3c7d9d40e0638024056de01212a235e6cd216474
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-28 21:55:18 -0400

    nooooooooo

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dece13976bbb7abd3c38a5b0ba2e96b7bdb41ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1541f95add18a58eed4ef33b67b1d9121ae8aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
